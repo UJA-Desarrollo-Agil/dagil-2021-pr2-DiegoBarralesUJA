af8f68edff1c50269b9f219959ef6b3fd7ad657b
dbc00009 <dbc00009@red.ujaen.es>
2021-03-08 19:38:07 +0100

    Cambios esteticos + otras cosas
    cambios estéticos y otros pequeños cambios

--- games/media/games/tutorial/Practica2.game.es.js
+++ games/media/games/tutorial/Practica2.game.es.js
@@ -29,8 +29,8 @@ undum.game.slideUpSpeed = 500
 /* The situations that the game can be in. Each has a unique ID. */
 undum.game.situations = {
     start: new undum.SimpleSituation(
-        "<h1>Starting Out with Undum</h1>\
-        <img src='media/games/tutorial/woodcut1.png' class='float_right'>\
+        "<h1>COMIENZO</h1>\
+        <img src='imagenes/tarta.jpg' width='500' height='320'>\
         <p>Welcome to the Undum tutorial. Undum is a tool for writing\
         hypertext interactive fiction. It has some unique features\
         and a visual design that encourages narrative games.</p>\
