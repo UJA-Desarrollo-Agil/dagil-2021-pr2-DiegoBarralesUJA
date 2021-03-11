d0009a59a91a5232e4f30f74724992ac17aefdaf
dbc00009 <dbc00009@red.ujaen.es>
2021-03-11 13:02:33 +0100

    Juego terminado
    Versión inicial del juego completo

--- games/media/games/tutorial/Practica2.game.es.js
+++ games/media/games/tutorial/Practica2.game.es.js
@@ -41,8 +41,6 @@ undum.game.situations = {
         y te encuentras muy cansado, por lo que tienes que decidir entre <a href='dormir'>dormir diez minutitos</a> o <a href='ir'>ir ya al supermercado</a>.</p>"
     ),
 
-
-
     "ir": new undum.SimpleSituation(
         "<p><img src='imagenes/mapache.jpg' width='500' height='260'></p>\
         <p>Has preferido ir al dichoso supermercado a pesar de que\
@@ -86,11 +84,15 @@ undum.game.situations = {
     ),
 
     "volver": new undum.SimpleSituation(
+
         "<p>Tras saber que no olvidas nada más, lo único que te queda es llegar al supermercado.\
         </p>\
         <p>El camino es largo, y no sabes si <a href='bus'>coger el bus</a> o <a href='andar'>ir andando</a> para quitarte el sueño que tenías.</p>",
         {
             heading: "volver a por la mascarilla",
+            enter: function(character, system, to){
+              system.setQuality("mascarilla", 1);
+            },
             displayOrder: 1,
             tags: ["topic"]
         }
@@ -104,6 +106,9 @@ undum.game.situations = {
         El camino es largo, y no sabes si <a href='bus'>coger el bus</a> o <a href='andar'>ir andando</a> para quitarte el sueño que tenías.</p>",
         {
             heading: "Revisar que lo llevas todo",
+            enter: function(character, system, to){
+              system.setQuality("mascarilla", 1);
+            },
             displayOrder: 1,
             tags: ["topic"]
         }
@@ -134,7 +139,7 @@ undum.game.situations = {
 
     "atajo": new undum.SimpleSituation(
         "<p><p><img src='imagenes/obras.jpg' width='500' height='320'>\
-        Vaya, por el atajo que tomas hay calle que está en obras y debes <a href='habitual'>volver al camino habitual</a>.</p>",
+        Vaya, por el atajo que tomas hay una calle en obras y debes <a href='habitual'>volver al camino habitual</a>.</p>",
         {
             heading: "tomar el atajo",
             displayOrder: 1,
@@ -160,18 +165,25 @@ undum.game.situations = {
         <p>Crees haberlo comprado todo, así que <a href='terminar1'>vuelves a casa</a> contento por haber hecho el recado casi sin problemas. </p>",
         {
             heading: "usar tu memoria",
+            enter: function(character, system, to){
+              system.setQuality("ingredientes", 1);
+            },
             displayOrder: 1,
             tags: ["topic"]
         }
     ),
 
     "whatsapp": new undum.SimpleSituation(
-        "<p>Sabes que la memoria no es tu fuerte.</p>\
+        "<p><img src='imagenes/whatsapp.jpg' width='500' height='320'></p>\
+        <p>Sabes que la memoria no es tu fuerte.</p>\
         <p>Así que aunque estés demostrándole a tu madre que eres un desastre, le pides la lista de ingredientes\
         y  finalmente consigues comprarlo todo. </p>\
         <p>Es hora de <a href='terminar2'>volver a casa</a> contento por haber hecho el recado sin problemas. </p>",
         {
             heading: "mandar un whatsapp a tu madre",
+            enter: function(character, system, to){
+              system.setQuality("ingredientes", 1);
+            },
             displayOrder: 1,
             tags: ["topic"]
         }
@@ -215,29 +227,18 @@ undum.game.start = "start";
  * possess. We don't have to be exhaustive, but if we miss one out then
  * that quality will never show up in the character bar in the UI. */
 undum.game.qualities = {
-    skill: new undum.IntegerQuality(
-        "Skill", {priority:"0001", group:'stats'}
-    ),
-    stamina: new undum.NumericQuality(
-        "Stamina", {priority:"0002", group:'stats'}
-    ),
-    luck: new undum.FudgeAdjectivesQuality( // Fudge as in the FUDGE RPG
-        "<span title='Skill, Stamina and Luck are reverently borrowed from the Fighting Fantasy series of gamebooks. The words representing Luck are from the FUDGE RPG. This tooltip is illustrating that you can use any HTML in the label for a quality (in this case a span containing a title attribute).'>Luck</span>",
-        {priority:"0003", group:'stats'}
-    ),
 
-    inspiration: new undum.NonZeroIntegerQuality(
-        "Inspiration", {priority:"0001", group:'progress'}
-    ),
-    novice: new undum.OnOffQuality(
-        "Novice", {priority:"0002", group:'progress', onDisplay:"&#10003;"}
-    ),
 
     mascarilla: new undum.OnOffQuality("Mascarilla",{
         priority:"0001",
-        group:'Cosas',
-        onDisplay:"&#10003;"})
+        group:'necesario',
+        onDisplay:"&#10003;"}),
 
+    ingredientes: new undum.YesNoQuality("Ingredientes",{
+        priority:"0001",
+        group:'necesario',
+        yesDisplay:"Sí",
+        noDisplay:"No"})
 };
 
 // ---------------------------------------------------------------------------
@@ -247,18 +248,14 @@ undum.game.qualities = {
  * the end. It is an error to have a quality definition belong to a
  * non-existent group. */
 undum.game.qualityGroups = {
-    stats: new undum.QualityGroup(null, {priority:"0001"}),
-    progress: new undum.QualityGroup('Progress', {priority:"0002"})
+    necesario: new undum.QualityGroup('Necesario', {priority:"0001"})
 };
 
 // ---------------------------------------------------------------------------
 /* This function gets run before the game begins. It is normally used
  * to configure the character at the start of play. */
 undum.game.init = function(character, system) {
-    character.qualities.skill = 12;
-    character.qualities.stamina = 12;
-    character.qualities.luck = 0;
-    character.qualities.novice = 1;
-    character.qualities.inspiration = 0;
-    system.setCharacterText("<p>¿Has cumplido el objetivo?.</p>");
+    system.setCharacterText("<p>¿Lo tienes todo?</p>");
+    character.qualities.mascarilla = 0;
+    character.qualities.ingredientes = 0;
 };
